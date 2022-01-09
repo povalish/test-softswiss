63a34432428b131007ebd5524583405f94ee3f4a
Nikita Povalishev <nikitapovalishev@Nikitas-MacBook-Pro.local>
2022-01-09 19:00:29 +0300

    Sort data before using

--- src/modules/games/utils/parsed.ts
+++ src/modules/games/utils/parsed.ts
@@ -2,8 +2,10 @@ import GamesJSON from '../games.json';
 import { Game } from '../types/Game';
 
 export const GAMES_TITLES = Object.keys(GamesJSON);
-export const PARSED_GAMES: Game[] = GAMES_TITLES.map((key) => ({
-  // @ts-ignore
-  ...GamesJSON[key],
-  key: key.replace('/', '-'),
-}));
+export const PARSED_GAMES: Game[] = GAMES_TITLES
+  .map((key) => ({
+    // @ts-ignore
+    ...GamesJSON[key],
+    key: key.replace('/', '-'),
+  }))
+  .sort((a, b) => a.collections.popularity - b.collections.popularity);
